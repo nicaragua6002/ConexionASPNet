98ea1be75f70b53d03ed2350f46daeda7f9e3eb2
Roberto Solís <nicaragua_6002@yahoo.es>
2023-10-02 14:55:16 -0600

    Agregar archivos de proyecto.

---
+++ ConexionASPNet/ConexionDB.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ConexionASPNet
+{
+   
+    public class ConexionDB : DbContext
+    {
+        public ConexionDB() : base("name=MiConexionDB") // El nombre coincide con la cadena de conexión definida en Web.config
+        {
+        }
+
+        // Define las entidades (tablas) de tu base de datos como propiedades DbSet.
+        public DbSet<Usuario> Usuarios { get; set; }
+        // Añade otras DbSet para las otras tablas de tu base de datos.
+
+        // Método para realizar consultas personalizadas si es necesario.
+        public List<Usuario> ObtenerUsuariosActivos()
+        {
+            return Usuarios.Where(u => u.Activo).ToList();
+        }
+    }
+
+    public class Usuario
+    {
+        int id;
+        string nombre;
+        string email;
+        string password;
+        Boolean activo;
+        public Usuario() { }
+        public Usuario(string nombre, string email, string password)
+        {
+
+            this.Nombre = nombre;
+            this.Email = email;
+            this.Password = password;
+            this.Activo = true;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public bool Activo { get => activo; set => activo = value; }
+    }
+}
---
+++ ConexionASPNet/Global.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConexionASPNet
+{
+    public class Global
+    {
+        //Instanciamiento a la clase ConexionDB
+        public static ConexionDB db = new ConexionDB();
+
+        //Listar a todos los usuarios contenidos en la tabla Usuarios de la BD
+        public static List<Usuario> ListarUsuarios() {
+            
+            return db.Usuarios.ToList();
+        } 
+
+        public static Boolean RegistrarUsuario(string nombre, string email, string pass)
+        {
+            db.Usuarios.Add(new Usuario(nombre, email, pass));
+            Boolean Result = true;
+            if(!(db.SaveChanges() > 0)){
+                Result = false;
+            }
+            return Result;
+        }
+
+        public static Boolean Login(string email, string pass)
+        {
+            if(db.Usuarios.Where(x=>x.Email==email && x.Password == pass).FirstOrDefault() !=null)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+        }
+    }
+}
